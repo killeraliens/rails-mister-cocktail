03b438d759696bdd6ca8aa7378c5a30181068be0
alexandra campbell <killeraliens@outlook.com>
2019-02-21 12:36:54 +0000

    model validation

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,6 @@
 class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+  validates_uniqueness_of :name
+  validates :name, presence: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ApplicationRecord
+  has_many :doses
+  validates_uniqueness_of :name
+  validates :name, presence: true
 end
