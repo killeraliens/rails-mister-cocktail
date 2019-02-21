4c80da6c21a87ebb60514a394476cd179ca72b96
alexandra campbell <killeraliens@outlook.com>
2019-02-21 22:15:30 +0000

    controllers/views for cocktails/doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@ require "json"
 
 puts "starting seed.."
 Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
 
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingred = open(url).read
@@ -13,4 +16,15 @@ ingredients["drinks"].each do |o|
   Ingredient.create(name: name )
 end
 
+10.times do
+  cocktail = Cocktail.create(name: Faker::Games::Pokemon.name)
+  5.times do
+    offset = rand(Ingredient.count)
+    rand_ingred = Ingredient.offset(offset).first
+    Dose.create(description: Faker::Food.measurement, cocktail: cocktail, ingredient: rand_ingred)
+  end
+end
+
+
 puts "ending seed"
+
