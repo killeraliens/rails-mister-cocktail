eed10b7914f54e3c9279ba2fa74a5c302c7529bd
alexandra campbell <killeraliens@outlook.com>
2019-02-21 12:54:44 +0000

    amount/description migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_120137) do
+ActiveRecord::Schema.define(version: 2019_02_21_124611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,11 @@ ActiveRecord::Schema.define(version: 2019_02_21_120137) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.integer "amount"
     t.bigint "ingredient_id"
     t.bigint "cocktail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
+    t.string "description"
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "starting seed.."
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingred = open(url).read
+ingredients = JSON.parse(ingred)
+puts ingredients[0]
+# 10.times do
+#   Ingredient.create()
+
+puts "ending seed"
