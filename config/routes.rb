484fcc9bf34d03571e4dc68c2041e6211a7c34bf
alexandra campbell <killeraliens@outlook.com>
2019-02-21 14:28:52 +0000

    cocktails doses controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
+
+#A user can see the list of cocktails
+#A user can see the details of a given cocktail, with the dose needed for each ingredient
+#A user can create a new cocktail
+
+# A user can add a new dose (ingredient/description pair) to an existing cocktail
+# Checkout simple_form docs about f.association to easily create a select dropdown for our list of ingredients.
+#A user can delete a dose that belongs to an existing cocktail. How can we make a delete link again?
