bb3343384840d818441f551d65f18e833560d942
alexandra campbell <killeraliens@outlook.com>
2019-02-28 08:50:29 +0000

    js click flip start

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,30 +4,14 @@ import 'bootstrap';
 
 console.log('Hello World from Webpacker')
 
-// const card = document.querySelector('.card-category');
+let cards = document.querySelectorAll('.card-category');
 
-// card.classList.add('animated', 'flipInY')
+cards.forEach(function (card) {
+  card.addEventListener('click', function (event) {
+    card.classList.add('animated', 'flipInY');
+  });
+});
 
-// card.addEventListener('animationend', function() { doSomething() })
-
-
-// const elm = document.querySelector('.card-category');
-
-// // First: get the current bounds
-// const first = elm.getBoundingClientRect();
-
-// // execute the script that causes layout change
-// doSomething();
-//triggerAnimation(card);
-
-// // Last: get the final bounds
-// const last = elm.getBoundingClientRect();
-
-// // Invert: determine the delta between the
-// // first and last bounds to invert the element
-// const deltaX = first.left - last.left;
-// const deltaY = first.top - last.top;
-// const deltaW = first.width / last.width;
-// const deltaH = first.height / last.height;
-
-
+  $(document).on('turbolinks:load', function(){
+      $('#show-card').addClass('animated fadeIn');
+  });
